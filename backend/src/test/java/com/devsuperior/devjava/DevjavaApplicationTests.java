352a8872858403eaade48ffc8c2eed4ab7b849cf
Thiago <thimachal@gmail.com>
2022-11-15 17:11:24 -0300

    Project created

---
+++ backend/src/test/java/com/devsuperior/devjava/DevjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.devjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
