f0c89587e3d1768e6994aa3719bd3792a19563df
Thiago <thimachal@gmail.com>
2022-11-15 19:07:10 -0300

    cabecalho

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,14 +1,15 @@
 import logo from '../../assets/img/logo.svg'
 
+import './style.css'
+
 function Header() {
     return (
         <header>
             <div className="dsmeta-logo-container">
                 <img src= {logo} alt="DSMeta" />
-                <h1>DSMeta</h1>
+                <h1>Dev Java</h1>
                 <p>
-                    Desenvolvido por
-                    <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
+                    Código de aula DeSuperior feito por<a href="https://www.linkedin.com/in/thiago-oliveira-tmo/"> Thiago Oliveira</a>
                 </p>
             </div>
         </header>
